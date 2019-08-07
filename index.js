5b6fbc7dd665e57fb8aa83d9f227ae47535094fc
T. Mjoen <trond@twined.net>
2019-08-07 11:39:42 +0200

    Update deps

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-var postcss = require('postcss');
-var assign = require('object-assign');
-var postcssImport = require('postcss-import');
-var isGlob = require('is-glob');
-var resolveGlob = require('./lib/resolve-glob');
-var resolveModule = require('./lib/resolve-module');
+var postcss = require('postcss')
+var assign = require('object-assign')
+var postcssImport = require('postcss-import')
+var isGlob = require('is-glob')
 
-function resolve(id) {
-    var resolver = isGlob(id) ? resolveGlob : resolveModule;
-    return resolver.apply(null, arguments);
+var resolveGlob = require('./lib/resolve-glob')
+var resolveModule = require('./lib/resolve-module')
+
+function resolve (id) {
+  var resolver = isGlob(id) ? resolveGlob : resolveModule
+  return resolver.apply(null, arguments)
 }
 
 module.exports = postcss.plugin('postcss-easy-import', function (opts) {
-    opts = assign({
-        prefix: false,
-        extensions: '.css'
-    }, opts);
+  opts = assign({
+    prefix: false,
+    extensions: '.css'
+  }, opts)
 
-    opts.resolve = resolve;
+  opts.resolve = resolve
 
-    if (opts.prefix && typeof opts.prefix !== 'string') {
-        throw Error(
-            'postcss-easy-import: ' +
+  if (opts.prefix && typeof opts.prefix !== 'string') {
+    throw Error(
+      'postcss-easy-import: ' +
             '\'prefix\' option should be a string or false'
-        );
-    }
+    )
+  }
 
-    if (typeof opts.extensions === 'string') {
-        opts.extensions = [opts.extensions];
-    }
+  if (typeof opts.extensions === 'string') {
+    opts.extensions = [opts.extensions]
+  }
 
-    var extensions = opts.extensions;
-    if (
-        !Array.isArray(extensions) ||
+  var extensions = opts.extensions
+  if (
+    !Array.isArray(extensions) ||
         !extensions.length ||
         extensions.filter(function (ext) {
-            return ext && typeof ext === 'string';
+          return ext && typeof ext === 'string'
         }).length !== extensions.length
-    ) {
-        throw Error(
-            'postcss-easy-import: ' +
+  ) {
+    throw Error(
+      'postcss-easy-import: ' +
             '\'extensions\' option should be string or array of strings'
-        );
-    }
+    )
+  }
 
-    return postcss([postcssImport(opts)]);
-});
+  return postcss([postcssImport(opts)])
+})
